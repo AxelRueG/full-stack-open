99a1bbf3a18804d24388bd71d994fa28f16ae4d5
Axel Rueda <axelcuer@gmail.com>
2022-01-29 17:35:48 -0300

    exercise 1.5

--- part1/course-information/src/index.js
+++ part1/course-information/src/index.js
@@ -18,30 +18,37 @@ const Content = (props) => {
 		</>
 	);
 };
-const Total = (props) => <p>Number of exercises {props.total}</p>;
+const Total = (props) => (
+	<p>
+		Number of exercises
+		{props.total.reduce((tot, elem) => (tot += elem.exercises), 0)}
+	</p>
+);
 
 const App = () => {
-	const course = 'Half Stack application development';
-	const parts = [
-		{
-			name: 'Fundamentals of React',
-			exercises: 10,
-		},
-		{
-			name: 'Using props to pass data',
-			exercises: 7,
-		},
-		{
-			name: 'State of a component',
-			exercises: 14,
-		},
-	];
+	const course = {
+		name: 'Half Stack application development',
+		parts: [
+			{
+				name: 'Fundamentals of React',
+				exercises: 10,
+			},
+			{
+				name: 'Using props to pass data',
+				exercises: 7,
+			},
+			{
+				name: 'State of a component',
+				exercises: 14,
+			},
+		],
+	};
 
 	return (
 		<div>
-			<Header course={course} />
-			<Content parts={parts} />
-			<Total total={parts.reduce((tot, elem) => (tot += elem.exercises), 0)} />
+			<Header course={course.name} />
+			<Content parts={course.parts} />
+			<Total total={course.parts} />
 		</div>
 	);
 };
