c611098be3961b221f3ecc6844602f176c8afe85
Axel Rueda <axelcuer@gmail.com>
2022-01-31 11:02:19 -0300

    exercise 2.19

--- part2/the-phonebook/src/App.js
+++ part2/the-phonebook/src/App.js
@@ -3,12 +3,17 @@ import Search from './components/Search';
 import FormNewNumber from './components/FormNewNumber';
 import Contacts from './components/Contacts';
 import Service from './services/services';
+import MessageAdded from './components/MessageAdded';
 
 const App = () => {
 	const [persons, setPersons] = useState([]);
 	const [newName, setNewName] = useState('');
 	const [newNumber, setNewNumber] = useState('');
 	const [search, setSearch] = useState('');
+	const [showMessage, setShowMessage] = useState({
+		status: false,
+		message: '',
+	});
 
 	useEffect(() => {
 		Service.getPersons().then((response) => {
@@ -22,14 +27,17 @@ const App = () => {
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		const existContact = persons.find((elem) => elem.name === newName);
+
 		if (existContact) {
 			if (window.confirm(`${newName} is already added to phonebook`)) {
 				const contactUpdate = { ...existContact, number: newNumber };
+
 				Service.updatePerson(existContact.id, contactUpdate)
 					.then((response) => response.data)
 					.then((response) => {
 						const rest = persons.filter((elem) => elem.id !== existContact.id);
 						setPersons(rest.concat(response));
+						setShowMessage({ status: true, message: 'Update number from ' });
 					});
 			}
 		} else {
@@ -38,7 +46,10 @@ const App = () => {
 				number: newNumber,
 			})
 				.then((response) => response.data)
-				.then((response) => setPersons(persons.concat(response)));
+				.then((response) => {
+					setPersons(persons.concat(response));
+					setShowMessage({ status: true, message: 'Added ' });
+				});
 		}
 		setNewName('');
 		setNewNumber('');
@@ -52,10 +63,22 @@ const App = () => {
 
 	const handleFilter = (e) => setSearch(e.target.value);
 
+	const handleShowMessage = () =>
+		setShowMessage({ status: false, message: '' });
+
 	return (
 		<div>
 			<h2>Phonebook</h2>
 			<Search search={search} handleFilter={handleFilter} />
+			{showMessage.status ? (
+				<MessageAdded
+					contact={persons[persons.length - 1]}
+					showMessage={handleShowMessage}
+					message={showMessage.message}
+				/>
+			) : (
+				<></>
+			)}
 			<FormNewNumber
 				name={newName}
 				number={newNumber}
